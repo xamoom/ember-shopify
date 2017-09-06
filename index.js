814ecf44fb13c8de2e8cb15e602be21d78081b15
Pavol Dano <pavol@xamoom.com>
2017-09-06 15:16:48 +0200

    make addon inject <script> to shopify-buy SDK on build

--- index.js
+++ index.js
@@ -2,5 +2,23 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-shopify'
+  name: 'ember-shopify',
+
+  contentFor(type, config) {
+
+    if (type !== 'head') {
+      return;
+    }
+    
+    if (!config['shopify-buy']) {
+      return;
+    }
+    
+    if (!config['shopify-buy'].enabled) {
+      return;
+    }
+
+    return `<script src="http://sdks.shopifycdn.com/js-buy-sdk/v0/latest/shopify-buy.umd.polyfilled.min.js"></script>`;
+  }
+
 };
