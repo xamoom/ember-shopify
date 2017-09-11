e2f556b305bef4a0f7b994611347e784beb6ed8c
Pavol Dano <pavol@xamoom.com>
2017-09-11 11:28:21 +0200

    reformat index.js file to meet ESLint rules although not tested

--- index.js
+++ index.js
@@ -1,24 +1,20 @@
-/* jshint node: true */
-'use strict';
-
 module.exports = {
   name: 'ember-shopify',
 
   contentFor(type, config) {
-
     if (type !== 'head') {
       return;
     }
-    
+
     if (!config['shopify-buy']) {
       return;
     }
-    
+
     if (!config['shopify-buy'].enabled) {
       return;
     }
 
-    return `<script src="http://sdks.shopifycdn.com/js-buy-sdk/v0/latest/shopify-buy.umd.polyfilled.min.js"></script>`;
-  }
+    return '<script src="http://sdks.shopifycdn.com/js-buy-sdk/v0/latest/shopify-buy.umd.polyfilled.min.js"></script>';
+  },
 
 };
